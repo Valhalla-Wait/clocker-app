7aa46c0d89742910d6b9859c340f2829b41b0909
Valhalla-Wait <miha888g@yandex.ru>
2023-08-21 08:11:31 +0300

    add interval timer logic

--- src/components/TimerDisplay/index.tsx
+++ src/components/TimerDisplay/index.tsx
@@ -2,9 +2,15 @@
 import { CircularProgress } from "@mui/material"
 import { LocalizationProvider, MultiSectionDigitalClock, TimeClock } from "@mui/x-date-pickers"
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns"
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { TimerFormates } from "../TimerConteiner/types"
 // // import {useState} from "React"
+
+type IntervalType = {
+    id: number
+    time: number
+    isPassed: boolean
+}
 
 export const TimerDisplay = ({ format }: {
     format: TimerFormates
@@ -12,26 +18,68 @@ export const TimerDisplay = ({ format }: {
     const [time, setTime] = useState(0)
     const [isPaused, setIsPaused] = useState(false)
     const [isActive, setIsActive] = useState(false)
-    const [intervals, setIntervals] = useState<number[]>([])
+    const [intervals, setIntervals] = useState<IntervalType[]>([])
+    const [intervalValue, setIntervalValue] = useState<number | null>(null)
+    const [currentInterval, setCurrentInterval] = useState<IntervalType | null>(null)
     const [clock, setClock] = useState<Date>(() => new Date())
     const countRef = useRef(0)
+
+    useEffect(() =>{
+        if(format === 'multi' && intervals.length && time === 0 && currentInterval) {
+            const nextInterval = intervals.find(interval => interval.id === currentInterval.id + 1)
+            if(!nextInterval) return handleReset()
+            setIntervals(prev => {
+                const prevIntervalIndex = prev.findIndex(i => i.id === currentInterval.id)
+                const copy = [...prev]
+                copy[prevIntervalIndex].isPassed = true
+                return copy
+            })
+            console.log(nextInterval)
+            debugger
+            if(nextInterval) {
+                setCurrentInterval(nextInterval)
+                setTime(nextInterval.time)
+            }
+        }
+    }, [time])
+
     const handleStart = () => {
         setIsActive(true)
         setIsPaused(false)
         if (format === 'interval') countRef.current = setInterval(() => {
             setTime((time) => time - 1)
         }, 1000)
+        else if(format === 'multi' && intervals.length){
+            setCurrentInterval(intervals[0])
+            setTime(intervals[0].time)
+            countRef.current = setInterval(() => setTime((time) => time - 1), 1000)
+        }
         else countRef.current = setInterval(() => {
             setTime((time) => time + 1)
         }, 1000)
     }
-    const handleIntervals = () => {
-        
+    const handleIntervals = (e:React.ChangeEvent<HTMLInputElement>) => {
+        const intervalTimeValue =Number(e.currentTarget.value) 
+        if(intervalTimeValue) {
+            setIntervalValue(intervalTimeValue)
+        }
     }
+
+    const addInterval = () => {
+        if(intervalValue) return setIntervals(prev => [...prev, {
+            id: prev.length,
+            time: intervalValue * 60,
+            isPassed:false
+        }])
+    }
+    console.log(intervals)
+    console.log(time)
 
     const handleReset = () => {
         setIsActive(false)
         setIsPaused(false)
+        setIntervals(prev => [...prev].map(i => ({...i, isPassed: false})))
+        setCurrentInterval(null)
         setTime(0)
         clearInterval(countRef.current)
     }
@@ -43,7 +91,7 @@ export const TimerDisplay = ({ format }: {
 
     const handleResume = () => {
         setIsPaused(false)
-        if (format === 'interval') countRef.current = setInterval(() => {
+        if (format === 'interval' || format === 'multi') countRef.current = setInterval(() => {
             setTime((time) => time - 1)
         }, 1000)
         else countRef.current = setInterval(() => {
@@ -53,11 +101,13 @@ export const TimerDisplay = ({ format }: {
     const handleInputTimer = (e: React.ChangeEvent<HTMLInputElement>) => {
         setTime(Number(e.currentTarget.value) * 60)
     }
-    const formatTime = () => {
-        const getSeconds = `0${(time % 60)}`.slice(-2)
-        const minutes = `${Math.floor(time / 60)}`
+    const formatTime = (seconds?:number) => {
+        const secs = seconds ? seconds : time
+
+        const getSeconds = `0${(secs % 60)}`.slice(-2)
+        const minutes = `${Math.floor(secs / 60)}`
         const getMinutes = `0${Number(minutes) % 60}`.slice(-2)
-        const getHours = `0${Math.floor(time / 3600)}`.slice(-2)
+        const getHours = `0${Math.floor(secs / 3600)}`.slice(-2)
 
         return `${getHours} : ${getMinutes} : ${getSeconds}`
     }
@@ -65,6 +115,16 @@ export const TimerDisplay = ({ format }: {
         <LocalizationProvider dateAdapter={AdapterDateFns}>
             <div className='grid justify-center justify-items-center'>
                 {format === 'interval' && <input type="text" placeholder="input min pls" onChange={handleInputTimer} />}
+                { format === 'multi' && 
+                <>
+                <input type="text" placeholder="add interval" onChange={handleIntervals} />
+                <button onClick={addInterval}>Add</button>
+                <div>
+                    Intervals:
+                    {intervals.map(i => <div className="grid justify-between">{formatTime(i.time)} {i.isPassed && "X"}</div>)}
+                </div>
+                </>
+                }
                 <CircularProgress variant="determinate" size={100} value={time} />
                 <br />
                 {formatTime()}
